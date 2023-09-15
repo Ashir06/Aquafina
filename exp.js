d44a41ecd6ece25134f328c763ea9794a06166a6
Ashir <ashirafzal188@gmail.com>
2023-09-14 20:40:22 -0400

    update

---
+++ exp.js
@@ -0,0 +1,7 @@
+var firstName = "Ashir"
+var lastName = "Afzal"
+var age = 17
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("I am having so much fun in this class");
